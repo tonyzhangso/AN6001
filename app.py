34b7e73f9261e309ff03648cf3070e219cc28f22
tonyzhangso <43349397+tonyzhangso@users.noreply.github.com>
2024-11-18 08:44:32 +0000

    5

--- app.py
+++ app.py
@@ -2,8 +2,15 @@
 from flask import Flask
 app = Flask(__name__)
 from flask import request, render_template
+
 @app.route("/", methods = ["GET", "POST"])
 def index():
     return(render_template('index.html'))
+
+@app.route("/main", methods = ["GET", "POST"])
+def main():
+    name = request.form.get("q")
+    return(render_template('main.html'))
+
 if __name__=="__main__":
     app.run()
